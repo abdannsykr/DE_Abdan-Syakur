d3244da14ea72528ffcf828e95b6e79b57c8a642
abdansyakur14002 <91769581+abdansyakur14002@users.noreply.github.com>
2024-02-27 22:02:25 +0700

    remed

--- 04.Basic Programming With Python/Praktikum/prioritas1no2.py
+++ 04.Basic Programming With Python/Praktikum/prioritas1no2.py
@@ -1,8 +1,7 @@
 phi = 3.14
 
-# Input jari-jari bola dari pengguna
-jari_jari = float(input("Masukkan jari-jari bola: "))
+jari_jari = float(input("Masukkan jari-jari : "))
 
 volume = (4/3) * phi * (jari_jari ** 3)
 
-print(f"Volume bola adalah : {volume}")
+print(f"Volume : {volume}")
