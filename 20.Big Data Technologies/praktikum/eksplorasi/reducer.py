b214c2b7e53981d17261e76efd84bb893edfc1dc
abdansyakur14002 <91769581+abdansyakur14002@users.noreply.github.com>
2024-04-23 22:20:33 +0700

    Tugas Big Data Technologies

--- 20.Big Data Technologies/praktikum/eksplorasi/reducer.py
+++ 20.Big Data Technologies/praktikum/eksplorasi/reducer.py
@@ -5,18 +5,14 @@ class PalindromeCheckerReducer:
     
     @staticmethod
     def process_input(line):
-        # Memisahkan baris input menjadi bagian-bagian
         line_parts = line.strip().split('\t')
-        
-        # Memeriksa apakah baris memiliki format yang diharapkan
+    
         if len(line_parts) == 2:
             word, is_palindrome = line_parts
             print(f"{word}\t{is_palindrome}")
         else:
-            # Mencetak pesan kesalahan ke stderr
-            print("Format input tidak valid:", line.strip(), file=sys.stderr)
+            print("Input tidak valid:", line.strip(), file=sys.stderr)
 
 if __name__ == "__main__":
-    # Membaca input dari standar input dan memprosesnya dengan reducer
     for line in sys.stdin:
         PalindromeCheckerReducer.process_input(line)
